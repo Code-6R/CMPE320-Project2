4b4261d0e31fe4588f9db3392380c4df2d2b5149
Gavin Roberts <gavinroberts061405@gmail.com>
2025-04-07 19:57:06 -0400

    multiple files now work, no files now prints the string wzip: file1 [file2 ...].

--- main.c
+++ main.c
@@ -1,31 +1,66 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+/*used https://www.geeksforgeeks.org/c-program-merge-contents-two-files-third-file/
+ * as a reference
+ */
+FILE *fileHandler(int argc, char *argv[]) {
+    FILE *fp[argc];
+    if (argc == 1) {
+        fprintf(stdout, "wzip: file1 [file2 ...]\n");
+        exit(EXIT_FAILURE); // exit(1)
+    }
+    for (int i = 1; i < argc; i++) {
+        fp[i] = fopen(argv[i], "r");
+    }
+    fp[0] = fopen("combined.txt", "w");
+    char ch;
+    int i = argc - 1;
+    while ( i != 0) {
+        while ((ch = fgetc(fp[i])) != EOF)
+            fputc(ch, fp[0]);
+        i--;
+    }
+    for (i = 0; i < argc; i++) {
+        fclose(fp[i]);
+    }
+    return fopen("combined.txt", "r");
+}
 
 int main(int argc, char *argv[]) {
-    int charPos = 0, j = 0, count = 0;
-    char *line = NULL;
-    FILE *input = NULL;
+    int j = 0, k = 0;
+    int count[10] = {0};
+    char *line = NULL, chr[10] = {};
+    FILE *input = fileHandler(argc, argv);
     size_t size = 0;
-    input = fopen(argv[1], "r");
-
     while (getline(&line, &size, input) != -1) {
-        line[strlen(line) - 1] = '\0';
+        if (line[strlen(line) - 1] == '\n')
+            line[strlen(line) - 1] = '\0';
+        int charPos = 0;
         while (line[charPos] != '\0') {
             j = charPos;
-            char chr = line[charPos];
-            while (chr == line[j]) {
+            chr[k] = line[charPos];
+            while (chr[k] == line[j])
                 j++;
+            count[k] = j - charPos;
+            if (chr[k] == chr[k - 1] && k > 0) {
+                count[k - 1] += count[k];
+                k--;
             }
-            j++;
-            count = j - charPos;
-            fwrite(&count, sizeof(count), 1, stdout);
-            fprintf(stdout, "%c", chr);
             charPos = j;
+            k++;
         }
-
-        fclose(input);
     }
+    count[k] = 0;
+    chr[k] = '\0';
+    k = 0;
+    while (chr[k] != '\0') {
+        fwrite(&count[k], 4, 1, stdout);
+        fprintf(stdout, "%c", chr[k]);
+        k++;
+    }
+    fclose(input);
     return 0;
 }
